f3a2cc47fa49fe60cf66a976eff6a2d9cff75730
Matthias Bock <mail@matthiasbock.net>
2022-01-27 17:33:33 +0100

    Implemented main class

---
+++ markdown_template.py
@@ -0,0 +1,22 @@
+from IPython.display import display, Markdown
+from string import Template
+
+#
+# Opens a Markdown formatted file
+# and replaces all $variables with
+# values from itself
+#
+class MarkdownTemplate():
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __str__(self):
+        f = open(self.filename, "r")
+        s = f.read()
+        f.close()
+        t = Template(s)
+        s = t.substitute(self.__dict__)
+        return s
+
+    def show(self):
+        display(Markdown(str(self)))

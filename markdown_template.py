b71c052dbff1a04d2d3bd6e03c5b6b70ede64e56
Matthias Bock <mail@matthiasbock.net>
2022-11-23 13:17:00 +0100

    Adding ability to load variables from JSON

--- markdown_template.py
+++ markdown_template.py
@@ -2,6 +2,9 @@
 # Use string library to parse values into template string/file
 from string import Template
 
+# For the ability to import variables from files
+import json
+
 # Generate output suitable for Jupyter notebook
 from IPython.display import display, Markdown
 
@@ -17,17 +20,51 @@ class MarkdownTemplate():
     #
     def __init__(self, s=None, filename=None):
         if filename != None:
-            f = open(filename, "r")
-            s = f.read()
-            f.close()
-        self.s = s
+            s = self.fromFile(filename)
+        self._template = s
+
+    #
+    # Load Markdown template from file
+    #
+    def fromFile(self, filename):
+        f = open(filename, "r")
+        self._template = f.read()
+        f.close()
+        return self._template
+
+    #
+    # Save filled Markdown template to file
+    #
+    def toFile(self, filename):
+        md = str(self)
+        f = open(filename, "w")
+        f.write(md)
+        f.close
+        return md
+
+    #
+    # Load template variable values from JSON file
+    #
+    def loadJSON(self, filename):
+        # Read file
+        f = open(filename, "r")
+        s = f.read()
+        f.close()
+
+        # Parse as JSON
+        j = json.loads(s)
+
+        # Copy values from JSON to this object
+        for key in j.keys():
+            if key != "_template":
+                self.__dict__[key] = j[key]
 
     #
     # Fill template with variables
     #
     def __str__(self):
         # Make a string.Template object
-        t = Template(self.s)
+        t = Template(self._template)
         # Subsitute template $variables with this object's attributes
         s = t.substitute(self.__dict__)
         return s
